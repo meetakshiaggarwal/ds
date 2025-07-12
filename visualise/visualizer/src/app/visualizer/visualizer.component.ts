0f491c029b6b277f240f29e599e6111ebbca4556
Meetakshi Aggarwal <meetakshi.a@nextbeemedia.com>
2025-07-12 18:49:08 +0530

    wip:working code for first three

--- visualise/visualizer/src/app/visualizer/visualizer.component.ts
+++ visualise/visualizer/src/app/visualizer/visualizer.component.ts
@@ -1,27 +1,26 @@
 import { Component, Input, OnChanges } from '@angular/core';
+
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common'; // For *ngFor, *ngIf, keyvalue pipe
 
 @Component({
   selector: 'app-visualizer',
   standalone: true,
-  imports: [CommonModule, FormsModule], // Ensure BrowserModule is imported if used
+  imports: [CommonModule, FormsModule],
   templateUrl: './visualizer.component.html',
   styleUrl: './visualizer.component.scss'
 })
-export class VisualizerComponent implements OnChanges {
+export class VisualizerComponent  implements OnChanges {
   @Input() steps: any[] = [];
   @Input() speed = 1000;
 
   currentStepIndex = 0;
   interval: any;
   playing = false;
-  private prevStep: any = null; // To store the previous step for comparison
 
   ngOnChanges(): void {
     this.currentStepIndex = 0;
     this.stop();
-    this.prevStep = null; // Reset prevStep when steps change
   }
 
   play() {
@@ -38,7 +37,6 @@ export class VisualizerComponent implements OnChanges {
 
   next() {
     if (this.currentStepIndex < this.steps.length - 1) {
-      this.prevStep = this.steps[this.currentStepIndex]; // Store current as previous
       this.currentStepIndex++;
     } else {
       this.pause();
@@ -48,17 +46,12 @@ export class VisualizerComponent implements OnChanges {
   prev() {
     if (this.currentStepIndex > 0) {
       this.currentStepIndex--;
-      // For 'prev', determine prevStep. If going back to index 0, prevStep is null.
-      this.prevStep = this.steps[this.currentStepIndex - 1] || null;
-    } else {
-      this.stop(); // If at the very beginning, stop and reset.
     }
   }
 
   stop() {
     this.pause();
     this.currentStepIndex = 0;
-    this.prevStep = null;
   }
 
   get currentStep() {
@@ -67,33 +60,5 @@ export class VisualizerComponent implements OnChanges {
 
   getKeys(obj: any): string[] {
     return Object.keys(obj || {});
-  }
-
-  // New method to check if a variable has changed
-  hasVariableChanged(key: string): boolean {
-    if (!this.prevStep || !this.currentStep) {
-      return false;
-    }
-    // Deep comparison for objects/arrays (like maps or result arrays)
-    if (typeof this.currentStep[key] === 'object' && this.currentStep[key] !== null) {
-      return JSON.stringify(this.prevStep[key]) !== JSON.stringify(this.currentStep[key]);
-    }
-    // Simple comparison for primitives
-    return this.prevStep[key] !== this.currentStep[key];
-  }
-
-  // New method to format variables for display (e.g., objects)
-  formatVariable(value: any): string {
-    if (value === undefined) {
-      return 'undefined';
-    }
-    if (value === null) {
-      return 'null';
-    }
-    if (typeof value === 'object') {
-      // For objects (like charFreqMap), stringify them nicely
-      return JSON.stringify(value, null, 2); // Pretty print JSON
-    }
-    return value.toString();
   }
 }
